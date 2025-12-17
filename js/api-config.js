3c3ee67390ee926641c5dae5be5d04b69e6a5996
unknown <juankeyboard@gmail.com>
2025-12-17 17:35:19 -0500

    actualización de API

--- js/api-config.js
+++ js/api-config.js
@@ -6,7 +6,7 @@
  */
 
 const API_CONFIG = {
-    GEMINI_API_KEY: 'AIzaSyDmSNF064-pEb50SQNOh7g2tXymOs4mMx8',
+    GEMINI_API_KEY: 'AIzaSyCvXVVmOxrAOjNXYORyXnqeoO8F2hamxwQ',
     GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent'
 };
 
